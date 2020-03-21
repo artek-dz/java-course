52ea59de9d1769b129c641b9a1fbe5e56f695e03
artek-dz <artek.dz3007@gmail.com>
2020-03-21 16:28:58 +0100

    assignment 3, updated versions of LetterPrint1, and LetterPrint2

--- src/assign3/LetterPrinter1.java
+++ src/assign3/LetterPrinter1.java
@@ -4,8 +4,8 @@ public class LetterPrinter1 {
     public static void main(String[] args) {
         if (args.length > 0) {
             char[] letters = args[0].toCharArray();
-            for (int i = 0; i < letters.length; i++) {
-                System.out.println(letters[i]);
+            for (char letter : letters) {
+                System.out.println(letter);
             }
         } else {
             System.out.println("No argument!");
--- src/assign3/LetterPrinter2.java
+++ src/assign3/LetterPrinter2.java
@@ -1,14 +1,19 @@
 package assign3;
 
+import java.util.Arrays;
+
 public class LetterPrinter2 {
     public static void main(String[] args) {
         if (args.length > 0) {
             String letters = args[0];
-            for (int i = 0; i < letters.length(); i++) {
-                System.out.println(letters.charAt(i));
+            int[] indexes = new int[letters.length()];
+            Arrays.setAll(indexes, index -> index);
+            for (int index : indexes) {
+                System.out.println(letters.charAt(index));
             }
         } else {
             System.out.println("No argument!");
         }
     }
 }
+
