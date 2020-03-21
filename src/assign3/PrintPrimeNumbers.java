1177007dd21696071f912568f9c0a9d46f894a81
artek-dz <artek.dz3007@gmail.com>
2020-03-21 18:53:06 +0100

    improved version, only checking for j<=sqrt(n)

--- src/assign3/PrintPrimeNumbers.java
+++ src/assign3/PrintPrimeNumbers.java
@@ -4,7 +4,7 @@ public class PrintPrimeNumbers {
     public static void main(String[] args) {
         for (int n = 2; n <= 100; n++) { // primes are greater than 1 i.e. 1 is not prime
             boolean isPrime = true;
-            for (int j = 2; j < n; j++) {
+            for (int j = 2; j <= Math.sqrt(n); j++) {
                 if (n % j == 0) {
                     isPrime = false;
                     break;
