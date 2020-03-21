5e905ef5fb48ee35439bbd5242648c5d5002b3f1
artek-dz <artek.dz3007@gmail.com>
2020-03-21 16:41:21 +0100

    assignment 3, SumCalculator

--- src/assign3/SumCalculator.java
+++ src/assign3/SumCalculator.java
@@ -1,4 +1,16 @@
 package assign3;
 
 public class SumCalculator {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            double sumOfArgs = 0;
+            for (String arg : args) {
+                sumOfArgs = sumOfArgs + Double.parseDouble(arg);
+            }
+
+            System.out.println(sumOfArgs);
+        } else {
+            System.out.println("No arguments provided!");
+        }
+    }
 }
