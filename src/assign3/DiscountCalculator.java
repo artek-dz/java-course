592ef9604ed17daa616aa424420892da7410815f
artek-dz <artek.dz3007@gmail.com>
2020-03-21 18:22:16 +0100

    assignment 3, DiscountCalculator

--- src/assign3/DiscountCalculator.java
+++ src/assign3/DiscountCalculator.java
@@ -1,13 +1,31 @@
 package assign3;
 
-import java.math.BigDecimal;
+
 import java.util.Scanner;
 
 public class DiscountCalculator {
     public static void main(String[] args) {
         System.out.print("Please provide item price: ");
-        BigDecimal price = new BigDecimal();
         Scanner userInput = new Scanner(System.in);
+        double price = userInput.nextDouble();
 
+        System.out.print("Please input customer age: ");
+        int age = userInput.nextInt();
+
+        double discount;
+
+        if (age < 6) {
+            discount = .80;
+        } else if (age <= 18) {
+            discount = .25;
+        } else if (age <= 65) {
+            discount = 0;
+        } else {
+            discount = .30;
+        }
+
+        double discounted = price * (1 - discount);
+
+        System.out.format("Discounted price: %.2f%n",discounted);
     }
 }
