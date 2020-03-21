363a03db6c2de84deee0aac81ad923135bf011df
artek-dz <artek.dz3007@gmail.com>
2020-03-21 17:26:32 +0100

    assignment 3, DiscountCalculator, v1

---
+++ src/assign3/DiscountCalculator.java
@@ -0,0 +1,13 @@
+package assign3;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class DiscountCalculator {
+    public static void main(String[] args) {
+        System.out.print("Please provide item price: ");
+        BigDecimal price = new BigDecimal();
+        Scanner userInput = new Scanner(System.in);
+
+    }
+}
