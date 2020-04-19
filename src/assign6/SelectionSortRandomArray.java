1d491729c5777f1cbc3d4150ed7b794bdb6f31a6
artek-dz <artek.dz3007@gmail.com>
2020-04-19 10:07:21 +0200

    assignment 6, Selection Sort for Lists

--- src/assign6/SelectionSortRandomArray.java
+++ src/assign6/SelectionSortRandomArray.java
@@ -5,33 +5,61 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class SelectionSortRandomArray {
     public static void main(String[] args) {
-        LinkedList<Integer> testList = new LinkedList<>();
-        for (int i = 0; i < 10; i++) {
-            testList.addLast(ThreadLocalRandom.current().nextInt(200));
+        List<Integer> linked = new LinkedList<>();
+        List<Integer> array = new ArrayList<>();
+        long start;
+        for (int i = 0; i < 2000; i++) {
+            Integer e = ThreadLocalRandom.current().nextInt(20000);
+            linked.add(e);
+            array.add(e);
         }
-
-        System.out.println(testList);
-        System.out.println(selectionSort(testList));
+        System.out.println("Linked list: " + linked);
+        System.out.println("===========================================");
+        start = System.currentTimeMillis();
+        System.out.println("Sorted list: " + selectionSort(linked));
+        System.out.format("Linked list sorted in %d millis \n", System.currentTimeMillis() - start);
+        System.out.println("===========================================");
+        start = System.currentTimeMillis();
+        System.out.println("Sorted list: " + selectionSort(array));
+        System.out.format("Array list sorted in %d millis \n", System.currentTimeMillis() - start);
     }
 
     public static LinkedList<Integer> selectionSort(LinkedList<Integer> list) {
-        LinkedList<Integer> sortedList = new LinkedList<>();
-        LinkedList<Integer> unsortedList = list;
+        LinkedList<Integer> sorted = new LinkedList<>();
         do {
-            Integer sorted = unsortedList.getFirst();
-            int sortedIndex = 0;
-            for (Integer unsorted : unsortedList) {
-                if (unsorted < sorted) {
-                    sorted = unsorted;
-                    sortedIndex = unsortedList.indexOf(unsorted);
+            Integer swapped = list.getFirst();
+            Integer minimal = list.getFirst();
+            int j = 0;
+            for (Integer element : list) {
+                if (element < minimal) {
+                    minimal = element;
+                    j = list.indexOf(element);
                 }
             }
-            sortedList.addLast(sorted);
+            sorted.addLast(minimal);
+            list.set(j, swapped);
+            list.removeFirst();
+        } while (!list.isEmpty());
+        return sorted;
+    }
 
-            unsortedList.set(sortedIndex, unsortedList.getFirst());
-            unsortedList.removeFirst();
-
-        } while (!unsortedList.isEmpty());
-        return sortedList;
+    private static List<Integer> selectionSort(List<Integer> list) {
+//        if (list instanceof LinkedList) {
+//            return selectionSort((LinkedList) list);
+//        }  //Comment out to optimize selection sorting for linked List
+        for (int i = 0; i < list.size(); i++) {
+            Integer swapped = list.get(i);
+            Integer minimal = list.get(i);
+            int j = i;
+            for (int k = i + 1; k < list.size(); k++) {
+                if (list.get(k) < minimal) {
+                    minimal = list.get(k);
+                    j = k;
+                }
+            }
+            list.set(i, minimal);
+            list.set(j, swapped);
+        }
+        return list;
     }
 }
