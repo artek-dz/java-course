3350bc7061cdc08c02598906309d60800f6db308
artek-dz <artek.dz3007@gmail.com>
2020-05-18 18:38:36 +0200

    assignment 10, UserNameGenerator v2

--- src/assign10/password/UserNameGenerator.java
+++ src/assign10/password/UserNameGenerator.java
@@ -1,10 +1,11 @@
 package assign10.password;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 public class UserNameGenerator {
     public String userName(Person person) {
-        return (person.getFirstName().substring(0, 1) + person.getLastName()).toLowerCase() +
-                (person.getBirthDate().isPresent() ? person.getBirthDate().get().getYear() : LocalDate.now().getYear());
+        return (person.getFirstName().charAt(0) + person.getLastName()).toLowerCase() +
+                Optional.ofNullable(person.getBirthDate()).orElse(LocalDate.now()).getYear();
     }
 }
